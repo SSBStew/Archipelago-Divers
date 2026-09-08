faf91ec961e15b9fb2f7e2d17cffdab5f100aa86
SSBStew <stevehopeross@gmail.com>
2026-09-08 13:14:03 -0400

    Add files via upload

---
+++ worlds/ufo50/games/divers/rules.py
@@ -0,0 +1,47 @@
+from typing import TYPE_CHECKING
+
+from BaseClasses import Region, CollectionState
+from worlds.generic.Rules import set_rule
+
+
+if TYPE_CHECKING:
+    from ... import UFO50World
+
+
+
+
+def create_rules(world: "UFO50World", regions: dict[str, Region]) -> None:
+    player = world.player
+    bomb = "Divers - Bomb"
+    brelic = "Divers - B relic"
+    menu = regions["Menu"]
+    shallows = regions["Shallows"]
+    ruins = regions["Ruins"]
+    depths = regions["Depths"]
+    boss = regions["Boss"]
+    menu.connect(shallows)
+
+    if not world.options.divers_lever_check:
+        shallows.connect(ruins)
+    else:
+        shallows.connect(ruins,
+                         rule=lambda state: state.has("Divers - Ruins Gate", player))
+    #ruins.connect(depths)
+    ruins.connect(depths,
+                  rule=lambda state: state.has_group("Divers - Weapons", player, 3) and state.has_group("Divers - Potions", player))
+
+    if not world.options.divers_lever_check:
+        depths.connect(boss,
+                       rule=lambda state: state.has("Divers - Mist Orb", player))
+    else:
+        depths.connect(boss,
+                       rule=lambda state: state.has("Divers - Mist Orb", player) and state.has("Divers - Ruins Gate",player) and state.has("Divers - Boss Gate 1", player) and state.has("Divers - Boss Gate 2", player))
+
+    set_rule(world.get_location("Divers - Shallows Bomb Blocked Chest 1"),
+             rule=lambda state: state.has(bomb, player))
+    set_rule(world.get_location("Divers - Shallows Bomb Blocked Chest 2"),
+             rule=lambda state: state.has(bomb, player))
+    set_rule(world.get_location("Divers - Piranha Cave Bomb Blocked Chest"),
+             rule=lambda state: state.has(bomb, player))
+    set_rule(world.get_location("Divers - Holy Potion Store"),
+             rule=lambda state: state.has(brelic, player))
